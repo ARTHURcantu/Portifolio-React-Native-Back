a2b00a6e3b5c1894d9a69382b8f9be4f028955e4
ARTHURcantu <cantuario.arthur@gmail.com>
2024-05-10 16:48:06 -0300

    my fist commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {PORT, HOST} from '../.en'
+import cors from 'cors'
+
+import logger from "./middleware/logger";
+
+const app = express()
+
+app.use(logger)
+app.use(cors({
+    origin:['http://localhost:3000', 'http://localhost:8081', 'http://meusite.com'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    a
+}))
+
+
